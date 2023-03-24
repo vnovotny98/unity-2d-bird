e8ef70d70d25b6684a43c7ecf638f2f1c0c3ab97
vnovotny98 <vnovotny@monetplus.cz>
2023-03-24 20:17:52 +0100

    Nastaveni Tagu + Layer pro komunikaci mezi scripty

--- skripty/Hra/ProselPipesScript.cs
+++ skripty/Hra/ProselPipesScript.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class ProselPipesScript : MonoBehaviour
 {
-    public SkoreScript TriggerSkore;
+    public SkoreScript logic;
     // Start is called before the first frame update
     void Start()
     {
-        TriggerSkore = GameObject.FindGameObjectWithTag("ReferencezProsel").GetComponent<SkoreScript>();
+        //Pripojeni logy ze peng prosel pipes s jinym skriptem pomoci kodu - proselpipes script reference na Skore Script
+        logic = GameObject.FindGameObjectWithTag("SkoreManTag").GetComponent<SkoreScript>();
     }
 
     // Update is called once per frame
@@ -20,7 +21,11 @@ public class ProselPipesScript : MonoBehaviour
     // funkce OnTrigger, pokazde kdyz projde trigger
     private void OnTriggerEnter2D(Collider2D collision)
     {
-            TriggerSkore.ProselPipes();
-
+        //Nastavil jsem novy layer na pengovi, layer3
+        if (collision.gameObject.layer == 3)
+        {
+            //Nastav funkci ProselPipes Odmenu 10
+            logic.ProselPipes(10);
+        }
     }
 }
--- skripty/Hra/SkoreScript.cs
+++ skripty/Hra/SkoreScript.cs
@@ -9,9 +9,10 @@ public class SkoreScript : MonoBehaviour
     public Text SkoreText;
 
     [ContextMenu("Prosel Pipes")]
-    public void ProselPipes()
+    public void ProselPipes(int Odmena)
     {
-        Skore = Skore + 1;
+        //Pridej Skore a Zapis to do Textoveho pole
+        Skore = Skore + Odmena;
         SkoreText.text = Skore.ToString();
     }
 }
